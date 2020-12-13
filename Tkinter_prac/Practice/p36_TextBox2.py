d50c0c5cd9567f2ac85d85ab173cdcf6fd477e37
Arijeet De <arijeetde@gmail.com>
2020-12-13 12:00:26 +0530

    p36

--- Tkinter_prac/Practice/p36_TextBox2.py
+++ Tkinter_prac/Practice/p36_TextBox2.py
@@ -22,14 +22,39 @@ def save_text():
     my_text.delete(1.0, END)
 
 
-my_text = Text(root, width=40, height=10, font=('Helvetica', 16))
-my_text.pack(pady=20)
+my_image = PhotoImage()
 
+
+def add_image():
+    # Add Image
+    global my_image
+    my_image = PhotoImage(file="../Projects/ImageViewer_Images/car.png")
+
+    position = my_text.index(INSERT)
+    my_text.image_create(position, image=my_image)
+
+
+my_frame = Frame(root)
+my_frame.pack(pady=10)
+
+# Create Scrollbar
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
+
+my_text = Text(my_frame, width=38, height=10, font=('Helvetica', 16), selectbackground="yellow", selectforeground="black", yscrollcommand=text_scroll.set)
+my_text.pack()
+
+# Configure the text _scroll
+text_scroll.config(command=my_text.yview)
+
+# Our Buttons
 open_button = Button(root, text="Open Text File", command=open_text)
 open_button.pack(pady=10)
 
 save_button = Button(root, text="Save", command=save_text)
 save_button.pack(pady=10)
 
+image_button = Button(root, text="Add Image", command=add_image)
+image_button.pack(pady=5)
 
 root.mainloop()
