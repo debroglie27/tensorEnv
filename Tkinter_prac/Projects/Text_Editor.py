43f7a2db93e2d1a8c12f687e0922299442c9df0c
Arijeet De <arijeetde@gmail.com>
2020-12-14 19:29:26 +0530

    Save As, New and Open Functunality

--- Tkinter_prac/Projects/Text_Editor.py
+++ Tkinter_prac/Projects/Text_Editor.py
@@ -1,10 +1,64 @@
 from tkinter import *
-# from tkinter import filedialog
+from tkinter import filedialog
 # from tkinter import font
 
 root = Tk()
-root.title('Text Editor')
+root.title('New File')
 root.geometry("900x536")
+
+filetypes = (("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*"))
+
+
+# Create a new file
+def new_file():
+    my_text.delete('1.0', END)
+    status_bar.config(text="New File      ")
+    root.title("New File")
+
+
+# Open a new file
+def open_file():
+    my_text.delete('1.0', END)
+
+    # Grab file
+    text_file = filedialog.askopenfilename(initialdir="./TextFiles/", title="Open File", filetypes=filetypes)
+
+    if text_file:
+        # Update Status Bar
+        status_bar.config(text=f'{text_file}      ')
+        name = text_file.replace("C:/Users/M K DE/PycharmProjects/tensorEnv/Tkinter_prac/Projects/TextFiles/", "")
+        root.title(name)
+
+        # Open the Actual File
+        text_file = open(text_file, 'r')
+        stuff = text_file.read()
+
+        # Inserting the Stuff in the opened file
+        my_text.insert(END, stuff)
+
+        # Close the Text File
+        text_file.close()
+
+
+# Save As File
+def save_as_file():
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="./TextFiles", title="Save File", filetypes=filetypes)
+
+    if text_file:
+        name = text_file
+
+        # Update Status Bar
+        status_bar.config(text=f'{text_file}      ')
+        name = name.replace("C:/Users/M K DE/PycharmProjects/tensorEnv/Tkinter_prac/Projects/TextFiles/", "")
+        root.title(name)
+
+        # Save the File
+        text_file = open(text_file, 'w')
+        text_file.write(my_text.get(1.0, END))
+
+        # Close the File
+        text_file.close()
+
 
 # Create Main Frame
 my_frame = Frame(root)
@@ -15,7 +69,8 @@ text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
 # Create Text Box
-my_text = Text(my_frame, width=73, height=21, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text = Text(my_frame, width=73, height=21, font=("Helvetica", 16), selectbackground="yellow",
+               selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
 my_text.pack()
 
 # Configure Scrollbar
@@ -28,9 +83,10 @@ root.config(menu=my_menu)
 # Add File Menu
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New")
-file_menu.add_command(label="Open")
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
 file_menu.add_command(label="Save")
+file_menu.add_command(label="Save As", command=save_as_file)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 
@@ -45,7 +101,7 @@ edit_menu.add_command(label="Undo")
 edit_menu.add_command(label="Redo")
 
 # Add Status Bar
-status_bar = Label(root, text='Ready  ', anchor=E)
+status_bar = Label(root, text='New File      ', anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=2)
 
 
