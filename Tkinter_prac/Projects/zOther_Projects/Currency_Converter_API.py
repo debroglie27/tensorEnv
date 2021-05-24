b986d9805b06631568604ba2918b1abcaf112b88
debroglie27 <arijeetde@gmail.com>
2021-05-24 21:08:11 +0530

    Currency Converter App

--- Tkinter_prac/Projects/zOther_Projects/Currency_Converter_API.py
+++ Tkinter_prac/Projects/zOther_Projects/Currency_Converter_API.py
@@ -1,30 +1,38 @@
-import requests
+import os
 import json
+import requests
 from tkinter import *
 from tkinter import messagebox
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_file_path = "C:/Users/HP/Pycharm_Projects/openCV_venv/.env"
+
+# Loading the Environment Variables from .env file
+env_path = Path(env_file_path)
+load_dotenv(dotenv_path=env_path)
+
+API_KEY = os.environ.get('CURRENCY_CONVERTER_API_KEY')
+
 
 # Getting Conversion Rate for two Particular Countries
-# api_request = requests.get("https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=00c0c507c9cea5e2cfc3")
+# api_request = requests.get("https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=" + API_KEY)
 # api = json.loads(api_request.content)
 # print(api)
 
 # Getting All Currency Details
-# api_request = requests.get("https://free.currconv.com/api/v7/currencies?apiKey=00c0c507c9cea5e2cfc3")
+# api_request = requests.get("https://free.currconv.com/api/v7/currencies?apiKey=" + API_KEY)
 # api = json.loads(api_request.content)
 # print(api['results'])
 
-api_countries_request = requests.get("https://free.currconv.com/api/v7/countries?apiKey=00c0c507c9cea5e2cfc3")
+api_countries_request = requests.get("https://free.currconv.com/api/v7/countries?apiKey=" + API_KEY)
 api = json.loads(api_countries_request.content)
-# print(api['results'])
 
 country_currency_dict = {}
 country_list = []
 for country_id in api['results']:
     country_currency_dict[api['results'][country_id]['name']] = api['results'][country_id]['currencyId']
     country_list.append(api['results'][country_id]['name'])
-
-# print(country_list)
-# print(country_currency_dict)
 
 root = Tk()
 root.title("Currency Converter App")
@@ -42,7 +50,7 @@ def conv_rate():
     else:
         search_option = country_currency_dict[country1_name] + '_' + country_currency_dict[country2_name]
         api_request = requests.get(
-            "https://free.currconv.com/api/v7/convert?q=" + search_option + "&compact=ultra&apiKey=00c0c507c9cea5e2cfc3")
+            "https://free.currconv.com/api/v7/convert?q=" + search_option + "&compact=ultra&apiKey=" + API_KEY)
         conversion_rate = json.loads(api_request.content)
 
         conversion_rate = str(round(float(conversion_rate[search_option]), 2))
@@ -140,6 +148,9 @@ my_list.bind("<<ListboxSelect>>", fill_out)
 
 # Create a binding on the Entry Box
 country1_entry.bind("<KeyRelease>", lambda event, obj=country1_entry: check(event, obj))
+country1_entry.bind("<Button-1>", lambda event, obj=country1_entry: check(event, obj))
 country2_entry.bind("<KeyRelease>", lambda event, obj=country2_entry: check(event, obj))
+country2_entry.bind("<Button-1>", lambda event, obj=country2_entry: check(event, obj))
+
 
 root.mainloop()
