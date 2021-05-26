12a1dbed738cc1a1f50085ccdf0b4dcc22e89d35
debroglie27 <arijeetde@gmail.com>
2021-05-26 11:29:31 +0530

    Made Improvements

--- Tkinter_prac/Projects/ImageViewer_App/ImageViewer.py
+++ Tkinter_prac/Projects/ImageViewer_App/ImageViewer.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
 root = Tk()
-root.title("Image Viewer")
+root.title("Image Viewer App")
+root.geometry("280x270+480+160")
+root.config(bg="#ebffa8")
+
 icon = PhotoImage(file='ImageViewer_Images/feather.png')
 root.iconphoto(False, icon)
-
-initial = 0
-i = initial - 1
 
 image1 = PhotoImage(file='ImageViewer_Images/car.png')
 image2 = PhotoImage(file='ImageViewer_Images/bike.png')
@@ -15,11 +15,10 @@ image4 = PhotoImage(file='ImageViewer_Images/plane.png')
 
 image_list = [image1, image2, image3, image4]
 
-status = Label(root, text=f"Image {i+1} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
-status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+i = 0
 
-my_label = Label(image=image_list[i])
-my_label.grid(row=0, column=0, columnspan=3)
+my_label = Label(image=image_list[i], bg="#ebffa8")
+my_label.pack(pady=(25, 0))
 
 
 def forward():
@@ -35,23 +34,27 @@ def forward():
 def backward():
     global i
     button_forward.config(state=NORMAL)
+
     i -= 1
-    status.config(text=f"Image {i+1} of {len(image_list)}")
     if i == 0:
         button_backward.config(state=DISABLED)
+
+    status.config(text=f"Image {i + 1} of {len(image_list)}")
     my_label.config(image=image_list[i])
 
 
-button_forward = Button(root, text=">>", command=forward)
-button_exit = Button(root, text="Exit Program", command=root.quit)
-if initial == 1:
-    button_backward = Button(root, text="<<", command=backward, state=DISABLED)
-else:
-    button_backward = Button(root, text="<<", command=backward)
+button_frame = Frame(root, bg="#ebffa8")
+button_frame.pack(pady=(30, 0))
 
+button_forward = Button(button_frame, text=">>", bg="#77fc65", font=("Helvetica", 10), command=forward)
+button_exit = Button(button_frame, text="Exit Program", bg="#fcc265", font=("Helvetica", 10), command=root.quit)
+button_backward = Button(button_frame, text="<<", bg="#77fc65", font=("Helvetica", 10), command=backward, state=DISABLED)
 
-button_forward.grid(row=1, column=2, pady=4)
-button_exit.grid(row=1, column=1)
-button_backward.grid(row=1, column=0)
+button_forward.grid(row=0, column=2)
+button_exit.grid(row=0, column=1, padx=10)
+button_backward.grid(row=0, column=0)
+
+status = Label(root, text=f"Image {i+1} of {len(image_list)}", bg="#dfff75", bd=1, relief=SUNKEN, anchor=E)
+status.pack(side=BOTTOM, fill=X)
 
 root.mainloop()
